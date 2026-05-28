7b3deef40ddd9ac7b22a203c7a887126972e2305
Mike Williams <mdub@dogbiscuit.org>
2026-05-28 10:49:23 +1000

    Update dbq to v0.4.3

--- Formula/dbq.rb
+++ Formula/dbq.rb
@@ -3,8 +3,8 @@
 class Dbq < Formula
   desc "Simple CLI for executing SQL queries against Databricks"
   homepage "https://github.com/mdub/dbq"
-  url "https://github.com/mdub/dbq/archive/refs/tags/v0.4.2.tar.gz"
-  sha256 "ed812009c12ceb885723562d482d2b6c6c24f98fc3e870f0768d6e13af63af4a"
+  url "https://github.com/mdub/dbq/archive/refs/tags/v0.4.3.tar.gz"
+  sha256 "72ddf51b4e8e212b74e2d6651ad8c6349f1380e3a984090d91e6e7f05867a942"
   depends_on "go" => :build
 
   def install
